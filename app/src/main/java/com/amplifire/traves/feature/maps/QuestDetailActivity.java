dd67a1d328c9b65f8aef93441d1d3523cf4eaa17
Danviero Yuzwan <yuzwan19@gmail.com>
2017-10-16 19:02:58 +0700

    quest detail activity
    detail quest

---
+++ app/src/main/java/com/amplifire/traves/feature/maps/QuestDetailActivity.java
@@ -0,0 +1,17 @@
+package com.amplifire.traves;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by user on 16/10/2017.
+ */
+
+public class QuestDetailActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quest_quiz);
+    }
+}
