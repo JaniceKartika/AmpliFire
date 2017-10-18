c9c4d8098b9bc67abfaa5c50f965e70ee7826382
Janice Kartika <janicekartika@gmail.com>
2017-10-18 11:30:18 +0700

    On click notification

--- app/src/main/java/com/amplifire/traves/feature/service/NotificationService.java
+++ app/src/main/java/com/amplifire/traves/feature/service/NotificationService.java
@@ -2,16 +2,21 @@ package com.amplifire.traves.feature.service;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 
 import com.amplifire.traves.App;
 import com.amplifire.traves.R;
+import com.amplifire.traves.feature.main.MainActivity;
+import com.amplifire.traves.feature.place.PlaceDetailActivity;
 import com.amplifire.traves.utils.FirebaseUtils;
+import com.amplifire.traves.utils.Utils;
 import com.firebase.geofire.GeoFire;
 import com.firebase.geofire.GeoLocation;
 import com.firebase.geofire.GeoQuery;
@@ -25,6 +30,8 @@ public class NotificationService extends Service {
 
     private static final int NOTIFICATION_ID = 42;
 
+    private String key = "";
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -37,6 +44,15 @@ public class NotificationService extends Service {
 
         NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
+        Intent action = new Intent(this, PlaceDetailActivity.class);
+        action.putExtra(Utils.DATA, key);
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(MainActivity.class);
+        stackBuilder.addNextIntent(action);
+
+        PendingIntent operation = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
         FirebaseRemoteConfig remoteConfig = App.mRemoteConfig;
         int radius = Integer.parseInt(remoteConfig.getString(FirebaseUtils.RADIUS));
 
@@ -47,11 +63,13 @@ public class NotificationService extends Service {
         geoQuery.addGeoQueryEventListener(new GeoQueryEventListener() {
             @Override
             public void onKeyEntered(String key, GeoLocation location) {
+                NotificationService.this.key = key;
                 Notification note = new NotificationCompat.Builder(NotificationService.this)
                         .setContentTitle(getString(R.string.notification_title))
                         .setContentText(getString(R.string.notification_text))
                         .setSmallIcon(R.mipmap.ic_launcher)
                         .setAutoCancel(true)
+                        .setContentIntent(operation)
                         .build();
 
                 manager.notify(NOTIFICATION_ID, note);
